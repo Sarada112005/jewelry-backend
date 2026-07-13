fe6806dadf5d6ec9258bc5782b188985ca8d90a4
Sarada112005 <msarada2005@gmai.com>
2026-07-13 21:09:43 +0530

    Add home API endpoint

--- jewelry_backend/urls.py
+++ jewelry_backend/urls.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+def home(request):
+    return JsonResponse({
+        "message": "Jewelry Backend API is running successfully!",
+        "products": "/api/products/",
+        "categories": "/api/categories/",
+        "admin": "/admin/"
+    })
